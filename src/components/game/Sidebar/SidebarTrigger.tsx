d1ce10dfe3f994db0371911d047da7248d157a7b
NavasardianMichael <navasardianmichael@gmail.com>
2025-10-06 01:49:56 +0400

    redundant fragment removed

--- src/components/game/Sidebar/SidebarTrigger.tsx
+++ src/components/game/Sidebar/SidebarTrigger.tsx
@@ -6,15 +6,13 @@ export default function SidebarTrigger() {
   const { toggleIsSidebarOpen } = useGameStore();
 
   return (
-    <>
-      <View className={`ml-auto`}>
-        <TouchableOpacity className="w-6 h-6" onPress={toggleIsSidebarOpen}>
-          <Image
-            source={require('../../../assets/icons/sidebar.svg')}
-            className={`w-full h-full mx-auto`}
-          />
-        </TouchableOpacity>
-      </View>
-    </>
+    <View className={`ml-auto`}>
+      <TouchableOpacity className="w-6 h-6" onPress={toggleIsSidebarOpen}>
+        <Image
+          source={require('../../../assets/icons/sidebar.svg')}
+          className={`w-full h-full mx-auto`}
+        />
+      </TouchableOpacity>
+    </View>
   );
 }
