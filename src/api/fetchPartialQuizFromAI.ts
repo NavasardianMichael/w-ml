b96e560ba707428219d042fec681e07c18111314
@michaelnavasardyan <michaelnavasardyan@espoc.com>
2025-10-30 02:26:22 +0400

    api ai

--- src/api/fetchPartialQuizFromAI.ts
+++ src/api/fetchPartialQuizFromAI.ts
@@ -1,10 +1,6 @@
-import { GoogleGenAI } from '@google/genai'
 import { QuizItem } from '@/store/game/types'
 import { Language } from '@/types/settings'
-
-const ai = new GoogleGenAI({
-  apiKey: '',
-})
+import { paramsToQueryString } from '@/helpers/commons'
 
 export type FetchQuizItemAPI = {
   payload: {
@@ -27,42 +23,23 @@ export const fetchPartialQuizFromAI = async ({
   language,
   difficulty,
   stagesRange,
-}: FetchQuizItemAPI['payload']): FetchQuizItemAPI['response'] => {
+}: FetchQuizItemAPI['payload']): Promise<FetchQuizItemAPI['response']> => {
   try {
-    const response = await ai.models.generateContent({
-      config: {
-        responseMimeType: 'application/json',
-        temperature: 0.9,
-        responseSchema: {
-          type: 'ARRAY',
-          items: {
-            type: 'OBJECT',
-            properties: {
-              question: { type: 'STRING' },
-              answerIndex: { type: 'NUMBER' },
-              options: {
-                type: 'ARRAY',
-                items: { type: 'STRING' },
-              },
-            },
-          },
-        },
-      },
-      model: 'gemini-2.5-flash',
-      contents: `We are playing "Who Wants to Be a Millionaire".
-      You are the game's question creator.
-      Create one question in ${language} language, the overall difficulty level of the question should be ${difficulty}. The question difficulty should be for stages ${stagesRange.start}-${stagesRange.end} out of 15. The question can be not only country or language-specific, but about a globally recognized topic as well (e.g., science, history, geography, arts, pop culture). Return a json, which has 3 properties: question (question text), options: (array of 4 string options), answerIndex: (index of the correct answer).
-      The answers should be short, unambiguous and without explanation.
-      Among the options there should be one correct and 3 wrong ones.`,
-    })
+    const response = await fetch(
+      `http://localhost:8080/genai/quiz-item?${paramsToQueryString({
+        language,
+        difficulty,
+        stagesRange,
+      })}`,
+    )
 
-    const jsonString = response?.candidates?.[0]?.content?.parts?.[0]?.text
-    if (!jsonString) {
-      throw new Error('No content found in API response.')
-    }
+    console.log({ response })
+    const jsonString = await response.json()
+    console.log({ jsonString })
 
-    const quizItem: FetchQuizItemAPI['response'] = JSON.parse(jsonString)
-    return quizItem
+    const quiz: FetchQuizItemAPI['response'] = JSON.parse(jsonString)
+    console.log({ quiz })
+    return quiz
   } catch (error) {
     console.error('Error generating word:', error)
   }
--- src/helpers/commons.ts
+++ src/helpers/commons.ts
@@ -1,3 +1,23 @@
 export const sleep = (ms: number): Promise<void> => {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
+
+export const paramsToQueryString = (params: Record<string, unknown>): string => {
+  if (!Object.keys(params).length) return ''
+
+  const allEntries: [string, string][] = []
+
+  for (const [key, value] of Object.entries(params)) {
+    if (Array.isArray(value)) {
+      value.forEach((item) => {
+        allEntries.push([key, String(item)])
+      })
+    } else if (value !== undefined && value !== null) {
+      allEntries.push([key, String(value)])
+    }
+  }
+
+  const queryString = new URLSearchParams(allEntries).toString()
+
+  return queryString
+}
